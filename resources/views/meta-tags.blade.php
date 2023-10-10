9e29f50b351f172846d3f824bf208a163ba65611
upiksaleh <upxsal@gmail.com>
2023-10-10 13:04:17 +0800

    update meta tags

---
+++ resources/views/meta-tags.blade.php
@@ -0,0 +1,8 @@
+<meta name="description" content="{{config('app.name_full')}}"/>
+<meta property="og:type" content="website"/>
+<meta property="og:url" content="{{URL::to('/')}}"/>
+<meta property="og:title" content="{{config('app.name')}}"/>
+<meta property="og:description" content="{{config('app.name_full')}}"/>
+<meta property="og:image" content="{{URL::to('/images/share-meta.jpeg')}}"/>
+<meta name="theme-color" content="#2563eb">
+<link rel="manifest" href="{{URL::to('/manifest.json')}}">
