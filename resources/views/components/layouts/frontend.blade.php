e6fbcc5e1680e3e7073312c3fa6b11dabc005c32
upiksaleh <upxsal@gmail.com>
2023-10-10 11:45:58 +0800

    add theme-color meta

--- resources/views/components/layouts/frontend.blade.php
+++ resources/views/components/layouts/frontend.blade.php
@@ -9,6 +9,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="theme-color" content="#2563eb">
   <link rel="manifest" href="{{URL::to('/manifest.json')}}">
   <title>{{$title}}</title>
   <x-google-font font="raleway"/>
